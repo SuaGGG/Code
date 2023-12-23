8c58ec948036f86be97e3a047e553e371bb4631d
SuaGGG <2321989255@qq.com>
2023-12-24 01:30:23 +0800

    main

---
+++ text.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+#define int long long
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define debug(x) cout<<#x<<" "<<x<<endl
+#define coutf(x) cout<<setiosflags(ios::fixed)<< setprecision ( x )
+
+using namespace std;
+
+const int N = 1e6 + 10;
+const int mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+
+void solve(){
+
+}
+
+signed main(){
+
+    /*freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);*/
+
+    ios::sync_with_stdio(0), cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+    
+}
