0b728aa53191f4a5c63411e323970c0e23e6c282
SuaGGG <2321989255@qq.com>
2023-12-24 01:58:05 +0800

    Code

--- text.cpp
+++ text.cpp
@@ -1,13 +1,10 @@
 #include<bits/stdc++.h>
 
 #define int long long
-#define endl '\n'
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-#define debug(x) cout << #x << " " << x << "\n"
-#define coutf(x) cout << setiosflags(ios::fixed) << setprecision ( x )
-
-using namespace std;
+#define debug(x) std::cout << #x << " " << x << "\n"
+#define coutf(x) std::cout << setiosflags(ios::fixed) << setprecision ( x )
 
 const int N = 1e6 + 10;
 const int mod = 1e9 + 7;
@@ -15,7 +12,7 @@ const int inf = 0x3f3f3f3f;
 const int INF = 0x3f3f3f3f3f3f3f3f;
 
 void solve() {
-
+  
 }
 
 signed main() {
@@ -23,9 +20,9 @@ signed main() {
   /*freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);*/
 
-  ios::sync_with_stdio(0), cin.tie(nullptr);
+  std::ios::sync_with_stdio(0), std::cin.tie(nullptr);
   int t = 1;
-  cin >> t;
+  //cin >> t;
   while (t--) solve();
   return 0;
   
